54da5b19df309748895dedcb781f4f4ed32925b9
Sanket Bansal <sanketbansal@Sankets-MacBook-Pro.local>
2016-10-15 15:14:55 +0530

    FIrst commit

---
+++ Assets/Scripts/sun.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sun : MonoBehaviour {
+
+	public bool houseFound = false;
+	GameObject sunObj;
+	// Use this for initialization
+	//Right now not in use pushed this code in defaultTrackableEventHandler
+	void Start () {
+		Debug.Log ("SUN START");
+		sunObj = GameObject.FindGameObjectWithTag("sun");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (houseFound) {
+			Debug.Log ("SUN UPDATE");
+			sunObj.transform.RotateAround (Vector3.zero, Vector3.right, 10f * Time.deltaTime);
+			sunObj.transform.LookAt (Vector3.zero);
+		}
+
+	}
+}
---
+++ Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -0,0 +1,130 @@
+/*==============================================================================
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+
+namespace Vuforia
+{
+    /// <summary>
+    /// A custom handler that implements the ITrackableEventHandler interface.
+    /// </summary>
+    public class DefaultTrackableEventHandler : MonoBehaviour,
+                                                ITrackableEventHandler
+    {
+        #region PRIVATE_MEMBER_VARIABLES
+ 
+        private TrackableBehaviour mTrackableBehaviour;
+		private Animation anim;
+    
+        #endregion // PRIVATE_MEMBER_VARIABLES
+
+
+
+        #region UNTIY_MONOBEHAVIOUR_METHODS
+    
+        void Start()
+        {
+            mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+            if (mTrackableBehaviour)
+            {
+                mTrackableBehaviour.RegisterTrackableEventHandler(this);
+            }
+        }
+
+        #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+
+
+        #region PUBLIC_METHODS
+
+        /// <summary>
+        /// Implementation of the ITrackableEventHandler function called when the
+        /// tracking state changes.
+        /// </summary>
+        public void OnTrackableStateChanged(
+                                        TrackableBehaviour.Status previousStatus,
+                                        TrackableBehaviour.Status newStatus)
+        {
+            if (newStatus == TrackableBehaviour.Status.DETECTED ||
+                newStatus == TrackableBehaviour.Status.TRACKED ||
+                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+            {
+                OnTrackingFound();
+            }
+            else
+            {
+                OnTrackingLost();
+            }
+        }
+
+        #endregion // PUBLIC_METHODS
+
+
+
+        #region PRIVATE_METHODS
+
+
+        private void OnTrackingFound()
+        {
+            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+            // Enable rendering:
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = true;
+            }
+
+            // Enable colliders:
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = true;
+            }
+
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+			if(mTrackableBehaviour.TrackableName.Equals("modernHouse1"))
+			{
+				//anim["rise"].speed = 0.5f;
+				GameObject.FindGameObjectWithTag("house").GetComponent<Animation>().Play("rise");
+
+				Debug.Log("Hello Sanket");
+
+				GameObject.FindGameObjectWithTag ("house_image").GetComponent<sun> ().houseFound = true;
+			}
+			else if(mTrackableBehaviour.TrackableName.Equals("FloorPlan"))
+			{
+				Debug.Log ("Floor plan found");
+				//anim["rise_floor"].speed = 0.5f;
+				GameObject.FindGameObjectWithTag("floorPlan").GetComponent<Animation>().Play("rise_floor");
+			}
+
+        }
+
+
+        private void OnTrackingLost()
+        {
+            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+            // Disable rendering:
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = false;
+            }
+
+            // Disable colliders:
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = false;
+            }
+
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+			GameObject.FindGameObjectWithTag ("house_image").GetComponent<sun> ().houseFound = false;
+        }
+
+        #endregion // PRIVATE_METHODS
+    }
+}
